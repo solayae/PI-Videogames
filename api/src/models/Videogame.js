0d24476d76426982208ae259d3c34f179ac23991
Sebas Olaya <sebastianolayae@gmail.com>
2022-12-27 20:33:51 -0500

    Avance BD

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,32 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
+// Exportamos una funcion que define el modelo.
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false, 
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },    
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.DATE
+    },
+    rating: {
+      type: DataTypes.DECIMAL
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.INTEGER),
+      allowNull: false
+    }
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const router = Router();
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
-const router = Router();
+const videogamesRouter = require("./videogames.js");
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.use("/videogames", videogamesRouter);
 
 module.exports = router;
