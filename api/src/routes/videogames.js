5679fa531dd5f6d94336302697a58dcc40ef2ad9
Sebas Olaya <sebastianolayae@gmail.com>
2023-01-03 18:06:48 -0500

    config para traer info de la api

--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -1,6 +1,18 @@
 const { Router } = require("express");
 const router = Router();
+const axios = require("axios");
 
 router.get("/", (req, res) => {
-  res.send("Hola")
-})
+  axios
+    .get("https://api.rawg.io/api/games?key=9f98323b4f174115a004ac5d4daf0fb3", { 
+      headers: { "Accept-Encoding": "gzip,deflate,compress" } 
+  })
+    .then((response) => {
+      res.send(response.data.results);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+module.exports = router;
